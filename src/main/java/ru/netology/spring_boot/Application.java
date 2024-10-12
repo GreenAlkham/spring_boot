1d0f9dc9b405e8bbd2f1df01b46c49816c6f0922
AlKham <greenowlkitty@gmail.com>
2024-10-12 21:35:17 +0500

    Init

---
+++ src/main/java/ru/netology/spring_boot/Application.java
@@ -0,0 +1,26 @@
+package ru.netology.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/ru/netology/spring_boot/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.spring_boot;
+
+public interface SystemProfile {
+    String getProfile();
+}
